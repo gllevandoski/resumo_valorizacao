0b5b6f7f7d3d3ad2f301dbb487f07f3a833b9bd7
Gabriel <gl.levandoskii@gmail.com>
2023-12-08 17:49:56 -0300

    improved code readability and now the code runs through every pdf in the samples folder

--- main.py
+++ main.py
@@ -2,92 +2,90 @@ def load_pdfs():
     from glob import glob
     import fitz
 
+    # top left coordinates (x0, y0) and bottom right coordinates (x1, y1)
     date_rect = (350, 80, 415, 100)
     representative_rect = (50, 80, 140, 95)
     buyer_rect = (150, 80, 360, 95)
     cpf_rect = (150, 110, 250, 120)
-    analysis_rect = (50, 250, 435, 475) # top left coordinates (x0, y0) and bottom right coordinates (x1, y1)
+    analysis_rect = (50, 250, 435, 475)
+    pdf_list = list()
 
     for file in glob("samples/**/*.pdf", recursive=True):
         pdf = fitz.open(file)
+
         for page in pdf:                
-            text = page.get_text("text", analysis_rect)
+            analysis = page.get_text("text", analysis_rect)
+            analysis_list = analysis_to_list(analysis)
+
             representative = page.get_text("text", representative_rect).strip()
             buyer = page.get_text("text", buyer_rect).strip()
             date = page.get_text("text", date_rect).strip()
             cpf = page.get_text("text", cpf_rect).strip()
-            dict_list = convert_pdf_to_dict(text, 6)
 
-            return {
-                        "date": date, 
-                        "representative": representative, 
-                        "buyer": buyer, 
-                        "cpf": cpf,
-                        "analysis": dict_list
-                    }
+            pdf_list.append({"date": date, "representative": representative, "buyer": buyer, "cpf": cpf, "analysis": analysis_list})
+
+    return pdf_list
 
 
-def convert_pdf_to_dict(text: list, column_count: int):
-    from string import digits
-    from math import ceil
+def analysis_to_list(analysis: list):
+    def cleanse_list(dirty_list: list):
+        from string import digits
+        clean_list = list()
 
-    split_text = text.split("\n")
-    list_text = list()
+        # searches the list for empty strings, dashes and non-useful stuff and removes it
+        for item in dirty_list:
+            for digit in digits:
+                if digit in item:
+                    clean_list.append(item)
+                    break
 
-    # cleaning up the split_text list from empty strings, 
-    # dashes and non-useful stuff
-    for item in split_text:
-        for digit in digits:
-            if digit in item:
-                list_text.append(item)
-                break
+        return clean_list
 
-    formated_text = list()
+    def format_list(unformatted_list: list):
+        from math import ceil
+        column_count = 6
+        formatted_list = list()
+        list_lenght = int(ceil(len(unformatted_list)) / column_count)
 
-    for i in range(int(ceil(len(list_text))/column_count)):
-        c = (i * column_count)
-        kg, pd, pt, rh, valor_un, valor_total = list_text[c:c + column_count]
-        formated_text.append(
-                                {
-                                    "kg": kg,
-                                    "pd": pd,
-                                    "pt": pt,
-                                    "rh": rh,
-                                    "valor_un": valor_un,
-                                    "valor_total": valor_total
-                                }
-                            )
+        # groups lists together depending on column_count
+        for i in range(list_lenght):
+            c = (i * column_count)
+            kg, pd, pt, rh, valor_un, valor_total = unformatted_list[c:c + column_count]
+            formatted_list.append({"kg": kg, "pd": pd, "pt": pt, "rh": rh, "valor_un": valor_un, "valor_total": valor_total})
 
-    return formated_text
+        return formatted_list
+
+
+    analysis_list = cleanse_list(analysis.split("\n"))
+    return format_list(analysis_list)
+
 
 def load_to_excel(pdf_info):
+    def return_last_empty_row(ss):
+        for row in ss.iter_rows(min_row=3, min_col=2, max_col=2):
+            if None is row[0].value:
+                return row[0].row
     from openpyxl import load_workbook
 
     wb = load_workbook("resumo.xlsx")
     spreadsheet = wb.active
 
-    for row in range(len(pdf_info["analysis"])):
-        date_cell = spreadsheet.cell(row + 3, 0 + 2)
-        representative_cell = spreadsheet.cell(row + 3, 1 + 2)
-        buyer_cell = spreadsheet.cell(row + 3, 2 + 2)
-        cpf_cell = spreadsheet.cell(row + 3, 3 + 2)
-        kg_cell = spreadsheet.cell(row + 3, 4 + 2)
-        pd_cell = spreadsheet.cell(row + 3, 5 + 2)
-        pt_cell = spreadsheet.cell(row + 3, 6 + 2)
-        rh_cell = spreadsheet.cell(row + 3, 7 + 2)
-        un_cell = spreadsheet.cell(row + 3, 8 + 2)
-        total_cell = spreadsheet.cell(row + 3, 9 + 2)
+    for pdf in pdf_info:
+        last_empty_row = return_last_empty_row(spreadsheet)
+        for i in range(len(pdf["analysis"])):
+            row = last_empty_row + i
 
-        date_cell.value = pdf_info["date"]
-        representative_cell.value = pdf_info["representative"]
-        buyer_cell.value = pdf_info["buyer"]
-        cpf_cell.value = pdf_info["cpf"]
-        kg_cell.value = float(pdf_info["analysis"][row]["kg"].replace(",", "."))
-        pd_cell.value = float(pdf_info["analysis"][row]["pd"].replace(",", "."))
-        pt_cell.value = float(pdf_info["analysis"][row]["pt"].replace(",", "."))
-        rh_cell.value = float(pdf_info["analysis"][row]["rh"].replace(",", "."))
-        un_cell.value = float(pdf_info["analysis"][row]["valor_un"].replace(",", "."))
-        total_cell.value = float(pdf_info["analysis"][row]["valor_total"].replace(",", "."))
+            spreadsheet.cell(row, 2).value = pdf["date"]
+            spreadsheet.cell(row, 3).value = pdf["representative"]
+            spreadsheet.cell(row, 4).value = pdf["buyer"]
+            spreadsheet.cell(row, 5).value = pdf["cpf"]
+            spreadsheet.cell(row, 6).value = float(pdf["analysis"][i]["kg"].replace(".", "").replace(",", "."))
+            spreadsheet.cell(row, 7).value = float(pdf["analysis"][i]["pd"].replace(".", "").replace(",", "."))
+            spreadsheet.cell(row, 8).value = float(pdf["analysis"][i]["pt"].replace(".", "").replace(",", "."))
+            spreadsheet.cell(row, 9).value = float(pdf["analysis"][i]["rh"].replace(".", "").replace(",", "."))
+            spreadsheet.cell(row, 10).value = float(pdf["analysis"][i]["valor_un"].replace(".", "").replace(",", "."))
+            spreadsheet.cell(row, 11).value = float(pdf["analysis"][i]["valor_total"].replace(".", "").replace(",", "."))
+
     wb.save("resumo.xlsx")
 
 
