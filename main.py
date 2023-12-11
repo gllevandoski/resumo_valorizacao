b08a293743c160a8f2263fec48a06aa6eee67816
Gabriel <gl.levandoskii@gmail.com>
2023-12-11 10:38:42 -0300

    added resuming the entries for each analysis
    • an optional argument in the function load_to_excel makes it possible
    for resuming the entries on each analysis
    • renamed the folder 'samples' to 'valorizacoes'

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def load_pdfs():
     analysis_rect = (50, 250, 435, 475)
     pdf_list = list()
 
-    for file in glob("samples/**/*.pdf", recursive=True):
+    for file in glob("valorizacoes/**/*.pdf", recursive=True):
         pdf = fitz.open(file)
 
         for page in pdf:                
@@ -23,6 +23,20 @@ def load_pdfs():
             cpf = page.get_text("text", cpf_rect).strip()
 
             pdf_list.append({"date": date, "representative": representative, "buyer": buyer, "cpf": cpf, "analysis": analysis_list})
+
+    for pdf in pdf_list:
+        kg, pd, pt, rh = 0, 0, 0, 0
+        
+        for analysis in pdf["analysis"]:
+            kg += float(analysis["kg"].replace(".", "").replace(",", "."))
+            pd += float(analysis["pd"].replace(".", "").replace(",", ".")) * float(analysis["kg"].replace(".", "").replace(",", "."))
+            pt += float(analysis["pt"].replace(".", "").replace(",", ".")) * float(analysis["kg"].replace(".", "").replace(",", "."))
+            rh += float(analysis["rh"].replace(".", "").replace(",", ".")) * float(analysis["kg"].replace(".", "").replace(",", "."))
+
+        pd /= kg
+        pt /= kg
+        rh /= kg
+        pdf["summary"] = {"kg": kg, "pd": pd, "pt": pt, "rh": rh}
 
     return pdf_list
 
@@ -60,7 +74,7 @@ def analysis_to_list(analysis: list):
     return format_list(analysis_list)
 
 
-def load_to_excel(pdf_info):
+def load_to_excel(pdf_info, resumed=False):
     def return_last_empty_row(ss):
         for row in ss.iter_rows(min_row=3, min_col=2, max_col=2):
             if None is row[0].value:
@@ -70,24 +84,40 @@ def load_to_excel(pdf_info):
     wb = load_workbook("resumo.xlsx")
     spreadsheet = wb.active
 
-    for pdf in pdf_info:
-        last_empty_row = return_last_empty_row(spreadsheet)
-        for i in range(len(pdf["analysis"])):
-            row = last_empty_row + i
+    if resumed:
+        for pdf in pdf_info:
+            row = return_last_empty_row(spreadsheet)
 
             spreadsheet.cell(row, 2).value = pdf["date"]
             spreadsheet.cell(row, 3).value = pdf["representative"]
             spreadsheet.cell(row, 4).value = pdf["buyer"]
             spreadsheet.cell(row, 5).value = pdf["cpf"]
-            spreadsheet.cell(row, 6).value = float(pdf["analysis"][i]["kg"].replace(".", "").replace(",", "."))
-            spreadsheet.cell(row, 7).value = float(pdf["analysis"][i]["pd"].replace(".", "").replace(",", "."))
-            spreadsheet.cell(row, 8).value = float(pdf["analysis"][i]["pt"].replace(".", "").replace(",", "."))
-            spreadsheet.cell(row, 9).value = float(pdf["analysis"][i]["rh"].replace(".", "").replace(",", "."))
-            spreadsheet.cell(row, 10).value = float(pdf["analysis"][i]["valor_un"].replace(".", "").replace(",", "."))
-            spreadsheet.cell(row, 11).value = float(pdf["analysis"][i]["valor_total"].replace(".", "").replace(",", "."))
+            spreadsheet.cell(row, 6).value = pdf["summary"]["kg"]
+            spreadsheet.cell(row, 7).value = pdf["summary"]["pd"]
+            spreadsheet.cell(row, 8).value = pdf["summary"]["pt"]
+            spreadsheet.cell(row, 9).value = pdf["summary"]["rh"]
+            # spreadsheet.cell(row, 10).value = pdf["analysis"]["valor_un"]
+            # spreadsheet.cell(row, 11).value = pdf["analysis"]["valor_total"]
+
+    if not resumed:
+        for pdf in pdf_info:
+            last_empty_row = return_last_empty_row(spreadsheet)
+            for i in range(len(pdf["analysis"])):
+                row = last_empty_row + i
+
+                spreadsheet.cell(row, 2).value = pdf["date"]
+                spreadsheet.cell(row, 3).value = pdf["representative"]
+                spreadsheet.cell(row, 4).value = pdf["buyer"]
+                spreadsheet.cell(row, 5).value = pdf["cpf"]
+                spreadsheet.cell(row, 6).value = float(pdf["analysis"][i]["kg"].replace(".", "").replace(",", "."))
+                spreadsheet.cell(row, 7).value = float(pdf["analysis"][i]["pd"].replace(".", "").replace(",", "."))
+                spreadsheet.cell(row, 8).value = float(pdf["analysis"][i]["pt"].replace(".", "").replace(",", "."))
+                spreadsheet.cell(row, 9).value = float(pdf["analysis"][i]["rh"].replace(".", "").replace(",", "."))
+                spreadsheet.cell(row, 10).value = float(pdf["analysis"][i]["valor_un"].replace(".", "").replace(",", "."))
+                spreadsheet.cell(row, 11).value = float(pdf["analysis"][i]["valor_total"].replace(".", "").replace(",", "."))
 
     wb.save("resumo.xlsx")
 
 
 pdf_information = load_pdfs()
-load_to_excel(pdf_information)
+load_to_excel(pdf_information, resumed=True)
