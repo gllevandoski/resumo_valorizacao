1eb0b1ae608f8ab0b74f0488f53666c95908d5da
Gabriel <gl.levandoskii@gmail.com>
2023-12-12 17:51:12 -0300

    just for saving work and continuing home
    in this commit i'm trying to make each folder in the valorizacoes folder
    be translated to a different worksheet, i guess the way to go is oop.

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+def str_to_int(string):
+    return float(string.replace(".", "").replace(",", "."))
+
+def get_spreadsheet(workbook, spreadsheet_name):
+    if spreadsheet_name not in workbook:
+        base = workbook["base"]
+        copied = workbook.copy_worksheet(base)
+        copied.title = spreadsheet_name
+    return workbook[spreadsheet_name]
+
 def load_pdfs():
     from glob import glob
+    from os import walk
     import fitz
 
     # top left coordinates (x0, y0) and bottom right coordinates (x1, y1)
@@ -8,37 +19,44 @@ def load_pdfs():
     buyer_rect = (150, 80, 360, 95)
     cpf_rect = (150, 110, 250, 120)
     analysis_rect = (50, 250, 435, 475)
-    pdf_list = list()
+    pdfs = dict()
 
-    for file in glob("valorizacoes/**/*.pdf", recursive=True):
-        pdf = fitz.open(file)
+    folders = list(walk("valorizacoes"))[0][1]
 
-        for page in pdf:                
-            analysis = page.get_text("text", analysis_rect)
-            analysis_list = analysis_to_list(analysis)
+    for folder in folders:
+        pdfs[folder] = list()
 
-            representative = page.get_text("text", representative_rect).strip()
-            buyer = page.get_text("text", buyer_rect).strip()
-            date = page.get_text("text", date_rect).strip()
-            cpf = page.get_text("text", cpf_rect).strip()
+        for file in glob(f"valorizacoes/{folder}/*.pdf"):
+            pdf = fitz.open(file)
 
-            pdf_list.append({"date": date, "representative": representative, "buyer": buyer, "cpf": cpf, "analysis": analysis_list})
+            for page in pdf:
+                analysis = page.get_text("text", analysis_rect)
+                analysis_list = analysis_to_list(analysis)
 
-    for pdf in pdf_list:
-        kg, pd, pt, rh = 0, 0, 0, 0
-        
-        for analysis in pdf["analysis"]:
-            kg += float(analysis["kg"].replace(".", "").replace(",", "."))
-            pd += float(analysis["pd"].replace(".", "").replace(",", ".")) * float(analysis["kg"].replace(".", "").replace(",", "."))
-            pt += float(analysis["pt"].replace(".", "").replace(",", ".")) * float(analysis["kg"].replace(".", "").replace(",", "."))
-            rh += float(analysis["rh"].replace(".", "").replace(",", ".")) * float(analysis["kg"].replace(".", "").replace(",", "."))
+                representative = page.get_text("text", representative_rect).strip()
+                buyer = page.get_text("text", buyer_rect).strip()
+                date = page.get_text("text", date_rect).strip()
+                cpf = page.get_text("text", cpf_rect).strip()
 
-        pd /= kg
-        pt /= kg
-        rh /= kg
-        pdf["summary"] = {"kg": kg, "pd": pd, "pt": pt, "rh": rh}
+                pdfs[folder][pdf].append({"date": date, "representative": representative, "buyer": buyer, "cpf": cpf, "analysis": analysis_list})
 
-    return pdf_list
+    for key, pdfs in pdfs.items():
+        for pdf in pdfs:
+            kg, pd, pt, rh = 0, 0, 0, 0
+            for analysis in pdf["analysis"]:
+                kg += str_to_int(analysis["kg"])
+                pd += str_to_int(analysis["pd"]) * str_to_int(analysis["kg"])
+                pt += str_to_int(analysis["pt"]) * str_to_int(analysis["kg"])
+                rh += str_to_int(analysis["rh"]) * str_to_int(analysis["kg"])
+
+            pd /= kg
+            pt /= kg
+            rh /= kg
+            pdf["summary"] = {"kg": kg, "pd": pd, "pt": pt, "rh": rh}
+
+    exit(0)
+
+    return pdfs
 
 
 def analysis_to_list(analysis: list):
@@ -82,39 +100,42 @@ def load_to_excel(pdf_info, resumed=False):
     from openpyxl import load_workbook
 
     wb = load_workbook("resumo.xlsx")
-    spreadsheet = wb.active
 
     if resumed:
-        for pdf in pdf_info:
-            row = return_last_empty_row(spreadsheet)
-
-            spreadsheet.cell(row, 2).value = pdf["date"]
-            spreadsheet.cell(row, 3).value = pdf["representative"]
-            spreadsheet.cell(row, 4).value = pdf["buyer"]
-            spreadsheet.cell(row, 5).value = pdf["cpf"]
-            spreadsheet.cell(row, 6).value = pdf["summary"]["kg"]
-            spreadsheet.cell(row, 7).value = pdf["summary"]["pd"]
-            spreadsheet.cell(row, 8).value = pdf["summary"]["pt"]
-            spreadsheet.cell(row, 9).value = pdf["summary"]["rh"]
-            # spreadsheet.cell(row, 10).value = pdf["analysis"]["valor_un"]
-            # spreadsheet.cell(row, 11).value = pdf["analysis"]["valor_total"]
-
-    if not resumed:
-        for pdf in pdf_info:
-            last_empty_row = return_last_empty_row(spreadsheet)
-            for i in range(len(pdf["analysis"])):
-                row = last_empty_row + i
+        for key, pdfs in pdf_info.items():
+            spreadsheet = get_spreadsheet(wb, key)
+            for pdf in pdfs:
+                row = return_last_empty_row(spreadsheet)
 
                 spreadsheet.cell(row, 2).value = pdf["date"]
                 spreadsheet.cell(row, 3).value = pdf["representative"]
                 spreadsheet.cell(row, 4).value = pdf["buyer"]
                 spreadsheet.cell(row, 5).value = pdf["cpf"]
-                spreadsheet.cell(row, 6).value = float(pdf["analysis"][i]["kg"].replace(".", "").replace(",", "."))
-                spreadsheet.cell(row, 7).value = float(pdf["analysis"][i]["pd"].replace(".", "").replace(",", "."))
-                spreadsheet.cell(row, 8).value = float(pdf["analysis"][i]["pt"].replace(".", "").replace(",", "."))
-                spreadsheet.cell(row, 9).value = float(pdf["analysis"][i]["rh"].replace(".", "").replace(",", "."))
-                spreadsheet.cell(row, 10).value = float(pdf["analysis"][i]["valor_un"].replace(".", "").replace(",", "."))
-                spreadsheet.cell(row, 11).value = float(pdf["analysis"][i]["valor_total"].replace(".", "").replace(",", "."))
+                spreadsheet.cell(row, 6).value = pdf["summary"]["kg"]
+                spreadsheet.cell(row, 7).value = pdf["summary"]["pd"]
+                spreadsheet.cell(row, 8).value = pdf["summary"]["pt"]
+                spreadsheet.cell(row, 9).value = pdf["summary"]["rh"]
+                # spreadsheet.cell(row, 10).value = pdf["analysis"]["valor_un"]
+                # spreadsheet.cell(row, 11).value = pdf["analysis"]["valor_total"]
+
+    if not resumed:
+        for key, pdfs in pdf_info.items():
+            spreadsheet = get_spreadsheet(wb, key)
+            for pdf in pdfs:
+                last_empty_row = return_last_empty_row(spreadsheet)
+                for i in range(len(pdf["analysis"])):
+                    row = last_empty_row + i
+
+                    spreadsheet.cell(row, 2).value = pdf["date"]
+                    spreadsheet.cell(row, 3).value = pdf["representative"]
+                    spreadsheet.cell(row, 4).value = pdf["buyer"]
+                    spreadsheet.cell(row, 5).value = pdf["cpf"]
+                    spreadsheet.cell(row, 6).value = str_to_int(pdf["analysis"][i]["kg"])
+                    spreadsheet.cell(row, 7).value = str_to_int(pdf["analysis"][i]["pd"])
+                    spreadsheet.cell(row, 8).value = str_to_int(pdf["analysis"][i]["pt"])
+                    spreadsheet.cell(row, 9).value = str_to_int(pdf["analysis"][i]["rh"])
+                    spreadsheet.cell(row, 10).value = str_to_int(pdf["analysis"][i]["valor_un"])
+                    spreadsheet.cell(row, 11).value = str_to_int(pdf["analysis"][i]["valor_total"])
 
     wb.save("resumo.xlsx")
 
